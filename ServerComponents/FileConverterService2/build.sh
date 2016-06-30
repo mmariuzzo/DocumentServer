825b1ff2745c88c98cd5b467e70435c9489e3209
Mauro Mariuzzo <mauro.mariuzzo@smc.it>
2016-06-30 15:55:25 +0200

    Fixed ServerComponents project details to allow rebuild

---
+++ ServerComponents/FileConverterService2/build.sh
@@ -0,0 +1,5 @@
+
+xbuild FileConverterService2.csproj /target:clean /property:Configuration=DebugOpenSource
+
+xbuild FileConverterService2.csproj /target:build /property:Configuration=DebugOpenSource
+
---
+++ ServerComponents/FileConverterUtils2/build.sh
@@ -0,0 +1,5 @@
+
+xbuild FileConverterUtils2.csproj /target:clean
+
+xbuild FileConverterUtils2.csproj /target:build
+
